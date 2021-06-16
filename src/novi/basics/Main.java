235da020b582297b6c5f4538be0822927a561c79
Wrens89 <r_89_9@hotmail.com>
2021-06-16 21:31:34 +0200

    test

---
+++ src/novi/basics/Main.java
@@ -0,0 +1,15 @@
+package novi.basics;
+
+public class Main {
+
+
+        public static void main(String[] args){
+                AppController app = new AppController();
+                app.generateCoach();
+                app.generatePerson();
+                app.generatePlayers();
+                app.printList();
+
+}
+}
+
